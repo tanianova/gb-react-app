4e3a21699e22ad239bb99d4a4ca5bcfcde07e097
Tatiana Tikhonova <tanianova@yandex.ru>
2022-01-21 18:02:36 +0300

    added propTypes, chatlist, input-autofocus; used Material UI(+theme)

--- src/components/SendMessageList.js
+++ src/components/SendMessageList.js
@@ -1,10 +1,14 @@
-const SendMessageList = ({ messages }) => {
+import { AUTHOR } from "../variables";
+import PropTypes from "prop-types";
+
+const SendMessageList = ({ messages = [] }) => {
   return (
     <ul className="message-container">
-      {messages.map((message, index) => {
-        const messageClassnames = message.author === "bot" ? "message-answer" : "";
+      {messages.map((message) => {
+        const messageClassnames =
+          message.author === AUTHOR.bot ? "message-answer" : "";
         return (
-          <li key={index} className={`message-item ${messageClassnames}`}>
+          <li key={message.id} className={`message-item ${messageClassnames}`}>
             <p className="message-author">{message.author}</p>
             <p className="message-text">{message.message}</p>
           </li>
@@ -14,4 +18,12 @@ const SendMessageList = ({ messages }) => {
   );
 };
 
+SendMessageList.propTypes = {
+  messagesList: PropTypes.arrayOf(
+    PropTypes.shape({
+      message: PropTypes.string,
+      author: PropTypes.string,
+    })
+  ),
+};
 export default SendMessageList;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createTheme, MuiThemeProvider } from "@material-ui/core/styles";
+import { STYLE_SETTINGS } from "./variables";
+import App from "./App";
 
-import App from './App';
-
+const theme = createTheme(STYLE_SETTINGS);
 
 ReactDOM.render(
-  <React.StrictMode>
+  <MuiThemeProvider theme={theme}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </MuiThemeProvider>,
+
+  document.getElementById("root")
 );
- 
